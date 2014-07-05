3ca5e07a347dea37d0d572228ed5373d94ab64ec
Jeremiah- <Jeremiah.H.Martinez@utexas.edu>
2014-07-05 01:26:23 -0500

    Added models for the datebase fixes #4

---
+++ world_cup/wc_app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from wc_app.models import *
+# Register your models here.
+
+admin.site.register(Country)
+admin.site.register(Player)
+admin.site.register(Match)
---
+++ world_cup/wc_app/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+class Country(models.Model):
+    country_name = models.CharField(max_length=200)
+    country_code = models.CharField(max_length=20)
+    rank = models.IntegerField(default=0)
+
+    def __str__ (self):
+        return self.country_name
+
+
+class Player(models.Model):
+    country = models.ForeignKey(Country)
+    sur_name = models.CharField(max_length=200)
+    full_name = models.CharField(max_length=200)
+    clubname = models.CharField(max_length=200)
+    position = models.CharField(max_length=64)
+    birth_date = models.DateTimeField()
+
+    def __str__ (self):
+        return self.full_name
+
+class Match(models.Model):
+    country_A = models.ForeignKey(Country, related_name='country_A')
+    country_B = models.ForeignKey(Country, related_name='country_B')
+    score = models.CharField(max_length=64)
+
+    def __str__ (self):
+        return country_A + " " + country_B + " " + score
