c285df143d917f053f964f5dd1c308a1fc817f41
Prithvi Shahi <pshahi92@cs.utexas.edu>
2014-07-06 19:56:39 -0500

    added nested url, close #31

--- src/mvp_landing/urls.py
+++ src/mvp_landing/urls.py
@@ -15,7 +15,19 @@ urlpatterns = patterns('',
     url(r'^about-us/$', 'signups.views.aboutus', name='aboutus'),
     url(r'^countries/$', 'signups.views.countries', name='countries'),
     url(r'^players/$', 'signups.views.players', name='players'),
-    url(r'^matches/$', 'signups.views.matches', name='matches'),
+    url(r'^matches/$', 'signups.views.matches', name='matches'),    
+    #countries
+    url(r'^countries/brazil/$', 'signups.views.brazil', name='brazil'),
+    url(r'^countries/argentina/$', 'signups.views.argentina', name='argentina'),
+    url(r'^countries/germany/$', 'signups.views.germany', name='germany'),    
+    #players
+    url(r'^players/messi/$', 'signups.views.messi', name='messi'),    
+    url(r'^players/neymar/$', 'signups.views.neymar', name='neymar'),    
+    url(r'^players/muller/$', 'signups.views.muller', name='muller'),
+    #matchess
+    url(r'^matches/arg_ned/$', 'signups.views.arg_ned', name='arg_ned'),    
+    url(r'^matches/ger_usa/$', 'signups.views.ger_usa', name='ger_usa'),    
+    url(r'^matches/bra_ger/$', 'signups.views.bra_ger', name='bra_ger'),
 )
 
 if settings.DEBUG:
--- src/signups/views.py
+++ src/signups/views.py
@@ -11,29 +11,61 @@ def home(request):
         messages.success(request,'Thanks, you have been successfully added!')
         return HttpResponseRedirect('/thank-you/')
         
-    return render_to_response("signup.html",locals(),context_instance=RequestContext(request))
+    return render_to_response("signup.html",
+        locals(),context_instance=RequestContext(request))
 
 def thankyou(request):
-
-        
-    return render_to_response("thankyou.html",locals(),context_instance=RequestContext(request))
+    return render_to_response("thankyou.html",
+        locals(),context_instance=RequestContext(request))
 
 def aboutus(request):
-
-        
-    return render_to_response("aboutus.html",locals(),context_instance=RequestContext(request))
+    return render_to_response("aboutus.html",
+        locals(),context_instance=RequestContext(request))
 
 def countries(request):
-
-        
-    return render_to_response("countries.html",locals(),context_instance=RequestContext(request))
+    return render_to_response("countries.html",
+        locals(),context_instance=RequestContext(request))
 
 def players(request):
-
-        
-    return render_to_response("players.html",locals(),context_instance=RequestContext(request))
+    return render_to_response("players.html",
+        locals(),context_instance=RequestContext(request))
 
 def matches(request):
+    return render_to_response("matches.html",
+        locals(),context_instance=RequestContext(request))
 
-        
-    return render_to_response("matches.html",locals(),context_instance=RequestContext(request))
+def brazil(request):
+    return render_to_response("brazil.html",
+        locals(),context_instance=RequestContext(request))
+
+def argentina(request):
+    return render_to_response("argentina.html",
+        locals(),context_instance=RequestContext(request))
+
+def germany(request):
+    return render_to_response("germany.html",
+        locals(),context_instance=RequestContext(request))
+
+def messi(request):
+    return render_to_response("messi.html",
+        locals(),context_instance=RequestContext(request))
+
+def muller(request):
+    return render_to_response("muller.html",
+        locals(),context_instance=RequestContext(request))
+
+def neymar(request):
+    return render_to_response("neymar.html",
+        locals(),context_instance=RequestContext(request))
+
+def bra_ger(request):
+    return render_to_response("bra_ger.html",
+        locals(),context_instance=RequestContext(request))
+
+def ger_usa(request):
+    return render_to_response("ger_usa.html",
+        locals(),context_instance=RequestContext(request))
+
+def arg_ned(request):
+    return render_to_response("arg_ned.html",
+        locals(),context_instance=RequestContext(request))
