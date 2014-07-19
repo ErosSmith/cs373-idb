81fb45c56769a632ac17c9ebf90d8e6cf6e9cc48
unknown <kimyu92@cs.utexas.edu>
2014-07-19 18:23:05 -0500

    country insert script insert changed for the updated model

--- world_cup/world_cup/settings.py
+++ world_cup/world_cup/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'mysql.connector.django',
-        'NAME' : 'testing_db3',  #this is mysql db name
+        'NAME' : 'testing_db4',  #this is mysql db name
         'USER': 'kimyu92',      #create a user for this db
         'PASSWORD': '123456',
         'HOST': 'localhost',
