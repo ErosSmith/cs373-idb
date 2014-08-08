bf8a38a03bbcb4d2b44f6266f3e1443197d9e1b0
unknown <kimyu92@cs.utexas.edu>
2014-08-07 22:01:11 -0500

    unit tests tweak and tests for search added

--- world_cup/world_cup/settings.py
+++ world_cup/world_cup/settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "watson.middleware.SearchContextMiddleware",
 )
 
 ROOT_URLCONF = 'world_cup.urls'
